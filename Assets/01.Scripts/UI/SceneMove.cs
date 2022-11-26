3e9faaef022e41f45d44b03c14309d91ff4ea3db
tjfdk <tjfdk6201@gmail.com>
2022-11-26 18:35:24 +0900

    introScene
    ㅇㅣㄴㅌㅡㄹㅗㅆㅣㄴ ㅁㅏㄴㄷㅡ
    인게임으로 넘어가게 연동해둠
    세팅판넬은 오늘 저녁에 다시 해두겠슴
    폰트 해결은 못 햇는데 적용은 해둠

---
+++ Assets/01.Scripts/UI/SceneMove.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour
+{
+    public void GameScene() {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
